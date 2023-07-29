ce5c64428fc1373890711d8c65423280e9590ade
Vishwas Moorjani <vishwasmoorjani02@gmail.com>
2023-07-30 01:43:05 +0530

    ADD: Detail Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 
+import Detail from './components/Detail'
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+
 function App() {
   return (
     <div className="App">
+
+      {/* <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router> */}
+
+      <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </Router>
+
+      
+      
     </div>
   );
 }
